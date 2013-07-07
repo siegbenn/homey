bb730179d3bb8f4e11e780218633dfb444ea9f7c
Bennett Siegel <bennett.e.siegel@gmail.com>
2013-07-07 17:41:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homey::Application.config.secret_key_base = 'c0abbe6be20731665cf388a56fb7059698a2b5348e649e429fe851e2ea61d4e74ed0443dc588d0056bf5a649f5147c06f92c39cc15bbfba4d07d99ac40c2e7af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Homey::Application.config.session_store :cookie_store, key: '_homey_session'
